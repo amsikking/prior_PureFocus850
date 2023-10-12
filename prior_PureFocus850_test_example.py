de9c6e6b185b6916ae92883fd6d3d6f198ff7469
Alfred Millett-Sikking <amsikking@gmail.com>
2023-10-12 13:47:57 -0700

    Update prior_PureFocus850_test_example.py
    Some tidy up and example code for how one might use the new 'get_sample_flag' method in 'prior_PureFocus850.py'.

--- prior_PureFocus850_test_example.py
+++ prior_PureFocus850_test_example.py
@@ -1,23 +1,28 @@
-import pi_E_709_1C1L
+import pi_E_709_1C1L        # github.com/amsikking/pi_E_709_1C1L
 import prior_PureFocus850
 
-piezo = pi_E_709_1C1L.Controller('COM15', 0, 800, verbose=True)
+piezo = pi_E_709_1C1L.Controller('COM27', 0, 800, verbose=True)
 autofocus = prior_PureFocus850.Controller(which_port='COM8', verbose=True)
 
 # Test focusing with serial port commands to the piezo:
-for i in range(10):
+z_um = 5
+for i in range(z_um):
     piezo.move_um(i)
-for i in range(10):
+for i in range(z_um):
     piezo.move_um(-i)
 
 # Prepare to switch piezo to analogue control mode:
-piezo.set_analog_control_limits(                # 0-10V is 0-300um range
-    v_min=0, v_max=10, z_min_ai=0, z_max_ai=300)
-z_voltage = piezo.get_voltage_for_move_um(0)    # get voltage for ~zero motion
-autofocus.set_piezo_range_um(300)               # pass piezo range to autofocus
-autofocus.set_piezo_voltage(z_voltage)          # set voltage for ~zero motion
+# -> consider picking z_range_um < objective working distance
+z_range_um = 300
+piezo.set_analog_control_limits(                # 0-10V is 'z_range_um'
+    v_min=0, v_max=10, z_min_ai=0, z_max_ai=z_range_um)
+autofocus.set_piezo_range_um(z_range_um)        # pass 'z_range_um' to autofocus
+z0_voltage = piezo.get_voltage_for_move_um(0)    # get voltage for ~zero motion
+autofocus.set_piezo_voltage(z0_voltage)          # set voltage for ~zero motion
 
 # Switch piezo to analogue control mode and enable autofocus:
+if not autofocus.get_sample_flag():                 # is a sample detected?
+    input('\n***WARNING***: sample flag=FALSE\n')
 input('\nTest stability (enter to continue)\n')     # gentle press on sample?
 piezo.set_analog_control_enable(True)               # switch piezo to analog
 autofocus.set_servo_enable(True)                    # enable autofocus
